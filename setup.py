2a0f7608e176f9dc4f27611f62ee3417dba1bdcf
Kevin <kevin.qi@berkeley.edu>
2021-11-16 16:24:55 -0800

    very minor updates

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import os
 from pathlib import Path
 import json
+from setuptools import setup, find_packages
 
+setup(name='HumanBat', version='1.0', packages=find_packages())
 
-def main():
-	# Get project root
-	project_root = str(Path(__file__).parent.resolve())
+#setup(name='humanbat', version='1.0', py_modules=['utils/extract_logger_data.py', 'utils/preprocess_video.py'])
 
-	# Read configuration file
-	with open('config/config.json', 'r') as f:
-		config = json.load(f)
+# Get project root
+project_root = str(Path(__file__).parent.resolve())
 
-	# Configure project root
-	config['project_root'] = project_root
-	print("Configured project root")
+# Read configuration file
+with open('config/config.json', 'r') as f:
+    config = json.load(f)
 
-	# Save to configuration file
-	with open('config.json', 'w') as f:
-		config = json.dump(config, f)
+# Configure project root
+config['project_root'] = project_root
+print("Configured project root")
 
-	print("Project setup successful!")
+# Save to configuration file
+with open('config.json', 'w') as f:
+    config = json.dump(config, f)
 
-if __name__ == '__main__':
-	main()
+print("Project setup successful!")
